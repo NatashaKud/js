91aaf38b498ca196334266d81890e5228f55e4ea
NatashaKud <cosmosliva2015@yandex.com>
2019-07-24 12:53:09 +0400

    ДЗ 1 по js

---
+++ script.js
@@ -0,0 +1,30 @@
+//вывод пяти переменных
+var age = 30;
+console.log(age);
+
+var name = "Наташа";
+console.log(name);
+
+var bool = true;
+console.log(bool);
+
+var nol = null;
+console.log(nol);
+
+var undefined = undefined;
+console.log(undefined);
+
+//вывод типа переменной
+console.log(typeof 30);
+
+console.log(typeof "Наташа");
+
+console.log(typeof true);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+//число во второй степени
+var a = prompt("Введите число");
+alert("Вторая степень числа " + a + " = " + a * a);
